a0568da2801bc1cf73353790927775fb9a57e8fc
hadirch <hadirich3@gmail.com>
2023-12-02 00:22:02 +0700

    add borrows controller

---
+++ Final Project/final-project/app/Controllers/Http/BorrowsController.ts
@@ -0,0 +1,60 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Borrow from 'App/Models/Borrow'
+import BorrowValidator from 'App/Validators/BorrowValidator'
+
+export default class BorrowsController {
+    public async index ({response}: HttpContextContract){
+        try {
+        const allBorrow = await Borrow
+        .query()
+        .preload('buku')
+        .preload('user')
+        return response.ok({
+            message: "Success fetch Borrows", 
+            data: allBorrow
+        })
+    } catch (error) {
+        return response.badRequest({
+          message: "Data Peminjaman gagal ditampilkan",
+          error: error.messages,
+        });
+      }
+    }
+
+    public async store({request, response, auth, params}: HttpContextContract){
+        const userid = auth.user?.id
+        const paramid = params.id
+        const payload = await request.validate(BorrowValidator)
+        
+        try {
+            let TPtoISo = payload.tanggal_pinjam
+            const [month, day, year] = TPtoISo.split('-');
+            const dates = new Date(+year, +month -1, +day)
+            let sbt = dates.toISOString()
+            let TglPinjamNew = sbt.substring(0,10)
+
+            let news = payload.tanggal_kembali
+            const [bulan, hari, tahun] = news.split('-');
+            const datesNew = new Date(+tahun, +bulan -1, +hari)
+            let substr = datesNew.toISOString()
+            let TglKembaliNew = substr.substring(0,10)
+        
+            const data = {
+                user_id: userid,
+                buku_id: paramid,
+                tanggal_pinjam: TglPinjamNew,
+                tanggal_kembali: TglKembaliNew,
+              }
+              await Borrow.create(data)
+            return response.created({message: "BERHASIL MELAKUKAN PEMINJAMAN"})
+        } catch (error) {
+            console.log(error)
+            if(error.guard){
+                return response.badRequest({message: 'GAGAL MELAKUKAN PEMINJAMAN',error: error.messages})
+            } else{
+                return response.badRequest({message: 'GAGAL MELAKUKAN PEMINJAMAN',error: error.message})
+            }
+        }
+    }
+    
+}
