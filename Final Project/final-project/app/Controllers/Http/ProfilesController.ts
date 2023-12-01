2e478397e1e96021f3685d6a76722c55c7dd48bc
hadirch <hadirich3@gmail.com>
2023-12-01 07:38:41 +0700

    add profileValidator & change profile to authController

--- Final Project/final-project/app/Controllers/Http/ProfilesController.ts
+++ Final Project/final-project/app/Controllers/Http/ProfilesController.ts
@@ -1,16 +1,16 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Profile from 'App/Models/Profile';
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+// import Profile from 'App/Models/Profile';
 
-export default class ProfilesController {
-    public async store({response, request, auth}: HttpContextContract){
-        const bio = request.input('bio');
-        const alamat = request.input('alamat');
-        Profile.updateOrCreate({
-            user_id: auth.user?.id
-        },{
-            bio: bio,
-            alamat: alamat
-        })
-        return response.created({message:"Profile Created"})
-    }
-}
+// export default class ProfilesController {
+//     public async store({response, request, auth}: HttpContextContract){
+//         const bio = request.input('bio');
+//         const alamat = request.input('alamat');
+//         Profile.updateOrCreate({
+//             user_id: auth.user?.id
+//         },{
+//             bio: bio,
+//             alamat: alamat
+//         })
+//         return response.created({message:"Profile Created"})
+//     }
+// }
--- Final Project/final-project/start/routes.ts
+++ Final Project/final-project/start/routes.ts
@@ -5,11 +5,12 @@ Route.get('/', async () => {
 })
 Route
   .group(() => {
+    Route.post('login', 'AuthController.login').as('auth.login')
+    Route.post('register', 'AuthController.register').as('auth.register')
+    Route.post('profile', 'AuthController.profile').as('profiles.store').middleware(['auth'])
+
     Route.resource('category', 'CategoriesController').apiOnly().middleware({'*': ['auth']})
     Route.resource('book', 'BooksController').apiOnly().middleware({'*': ['auth']})
     Route.resource('borrow', 'BorrowsController').apiOnly().middleware({'store':['auth']})
-    Route.post('register', 'AuthController.register').as('auth.register')
-    Route.post('login', 'AuthController.login').as('auth.login')
-    Route.post('profile', 'ProfilesController.store').as('profiles.store').middleware(['auth'])
   })
   .prefix('/api/v1/')
