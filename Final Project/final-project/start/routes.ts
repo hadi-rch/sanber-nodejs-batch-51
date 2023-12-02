d1264ff8ccf08a617fd40d404a539acbdd85c3b0
hadirch <hadirich3@gmail.com>
2023-12-02 16:56:15 +0700

    update routes

--- Final Project/final-project/start/routes.ts
+++ Final Project/final-project/start/routes.ts
@@ -7,13 +7,26 @@ Route
   .group(() => {
     Route.post('login', 'AuthController.login').as('auth.login')
     Route.post('register', 'AuthController.register').as('auth.register')
+    Route.post('verifikasi-otp', 'AuthController.otpConfirmation').as('auth.otpConfirmation')
     Route.post('profile', 'AuthController.profile').as('profiles.store').middleware(['auth'])
 
-    Route.resource('category', 'CategoriesController').apiOnly().middleware({'*': ['auth']})
-    Route.resource('book', 'BooksController').apiOnly().middleware({'*': ['auth']})
-    Route.resource('borrow', 'BorrowsController').apiOnly().middleware({'store':['auth']})
-    Route.post('/buku/:id/peminjaman', 'BorrowsController.store').middleware(['auth'])
-    Route.post('/peminjaman/:id', 'BorrowsController.index').middleware(['auth'])
+    Route.resource('category', 'CategoriesController').apiOnly().middleware({
+      'index': ['auth', 'verify'],
+      'store': ['auth', 'verify', 'admin'],
+      'show': ['auth', 'verify'],
+      'update': ['auth', 'verify', 'admin'],
+      'destroy': ['auth', 'verify', 'admin'],
+    })
+    Route.resource('book', 'BooksController').apiOnly().middleware({
+      'index': ['auth', 'verify'],
+      'store': ['auth', 'verify', 'admin'],
+      'show': ['auth', 'verify'],
+      'update': ['auth', 'verify', 'admin'],
+      'destroy': ['auth', 'verify', 'admin'],
+    })
+    Route.get('peminjaman', 'BorrowsController.index').middleware(['auth', 'verify', 'admin'])
+    Route.post('buku/:id/peminjaman', 'BorrowsController.store').middleware(['auth','verify','user'])
+    Route.get('peminjaman/:id', 'BorrowsController.show').middleware(['auth','verify','admin'])
 
   })
   .prefix('/api/v1/')
